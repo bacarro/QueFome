a0eedc4de5a69747b171c7c1696de6c920ba094b
bacarro <111158706+bacarro@users.noreply.github.com>
2023-05-24 16:36:51 -0300

    Add files via upload

--- QueFome/QueFome/Apresentação/FormProdutos.Designer.cs
+++ QueFome/QueFome/Apresentação/FormProdutos.Designer.cs
@@ -28,20 +28,197 @@
         /// </summary>
         private void InitializeComponent()
         {
+            comboBox1 = new ComboBox();
+            btn_adicionar = new Button();
+            bt_excluir = new Button();
+            lv_produto = new ListView();
+            col_id = new ColumnHeader();
+            col_grupo = new ColumnHeader();
+            col_nome = new ColumnHeader();
+            col_preco = new ColumnHeader();
+            txt_id = new TextBox();
+            txt_grupo = new TextBox();
+            txt_nome = new TextBox();
+            txt_preco = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
             SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Todos os itens", "Combos", "Sanduíches", "Bebidas" });
+            comboBox1.Location = new Point(136, 41);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(205, 28);
+            comboBox1.TabIndex = 0;
+            comboBox1.Text = "Todos os Itens";
+            // 
+            // btn_adicionar
+            // 
+            btn_adicionar.ForeColor = Color.Black;
+            btn_adicionar.Location = new Point(152, 516);
+            btn_adicionar.Name = "btn_adicionar";
+            btn_adicionar.Size = new Size(157, 50);
+            btn_adicionar.TabIndex = 1;
+            btn_adicionar.Text = "Adicionar";
+            btn_adicionar.UseVisualStyleBackColor = true;
+            btn_adicionar.Click += btn_adicionar_Click;
+            // 
+            // bt_excluir
+            // 
+            bt_excluir.ForeColor = Color.Black;
+            bt_excluir.Location = new Point(574, 516);
+            bt_excluir.Name = "bt_excluir";
+            bt_excluir.Size = new Size(156, 50);
+            bt_excluir.TabIndex = 2;
+            bt_excluir.Text = "Excluir";
+            bt_excluir.UseVisualStyleBackColor = true;
+            bt_excluir.Click += bt_excluir_Click;
+            // 
+            // lv_produto
+            // 
+            lv_produto.Columns.AddRange(new ColumnHeader[] { col_id, col_grupo, col_nome, col_preco });
+            lv_produto.FullRowSelect = true;
+            lv_produto.GridLines = true;
+            lv_produto.Location = new Point(136, 115);
+            lv_produto.Name = "lv_produto";
+            lv_produto.Size = new Size(613, 305);
+            lv_produto.TabIndex = 3;
+            lv_produto.UseCompatibleStateImageBehavior = false;
+            lv_produto.View = View.Details;
+            lv_produto.SelectedIndexChanged += lv_produto_SelectedIndexChanged;
+            // 
+            // col_id
+            // 
+            col_id.Text = "ID";
+            // 
+            // col_grupo
+            // 
+            col_grupo.Text = "Grupo";
+            col_grupo.Width = 150;
+            // 
+            // col_nome
+            // 
+            col_nome.Text = "Nome";
+            col_nome.Width = 300;
+            // 
+            // col_preco
+            // 
+            col_preco.Text = "Preço";
+            col_preco.Width = 100;
+            // 
+            // txt_id
+            // 
+            txt_id.Location = new Point(136, 450);
+            txt_id.Name = "txt_id";
+            txt_id.Size = new Size(58, 27);
+            txt_id.TabIndex = 4;
+            // 
+            // txt_grupo
+            // 
+            txt_grupo.Location = new Point(200, 450);
+            txt_grupo.Name = "txt_grupo";
+            txt_grupo.Size = new Size(150, 27);
+            txt_grupo.TabIndex = 5;
+            // 
+            // txt_nome
+            // 
+            txt_nome.Location = new Point(356, 450);
+            txt_nome.Name = "txt_nome";
+            txt_nome.Size = new Size(292, 27);
+            txt_nome.TabIndex = 6;
+            // 
+            // txt_preco
+            // 
+            txt_preco.Location = new Point(654, 450);
+            txt_preco.Name = "txt_preco";
+            txt_preco.Size = new Size(76, 27);
+            txt_preco.TabIndex = 7;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.Gainsboro;
+            label1.Location = new Point(136, 427);
+            label1.Name = "label1";
+            label1.Size = new Size(24, 20);
+            label1.TabIndex = 8;
+            label1.Text = "ID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.Gainsboro;
+            label2.Location = new Point(200, 427);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 9;
+            label2.Text = "Grupo";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.Gainsboro;
+            label3.Location = new Point(356, 427);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 10;
+            label3.Text = "Nome";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(654, 427);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 20);
+            label4.TabIndex = 11;
+            label4.Text = "Preço";
             // 
             // FormProdutos
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Orange;
-            ClientSize = new Size(989, 587);
-            ForeColor = SystemColors.ControlText;
+            BackColor = Color.Black;
+            ClientSize = new Size(1092, 630);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txt_preco);
+            Controls.Add(txt_nome);
+            Controls.Add(txt_grupo);
+            Controls.Add(txt_id);
+            Controls.Add(lv_produto);
+            Controls.Add(bt_excluir);
+            Controls.Add(btn_adicionar);
+            Controls.Add(comboBox1);
+            ForeColor = Color.Gainsboro;
             Name = "FormProdutos";
             Text = "Produtos";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private Button btn_adicionar;
+        private Button bt_excluir;
+        private ListView lv_produto;
+        private ColumnHeader col_id;
+        private ColumnHeader col_grupo;
+        private ColumnHeader col_nome;
+        private ColumnHeader col_preco;
+        private TextBox txt_id;
+        private TextBox txt_grupo;
+        private TextBox txt_nome;
+        private TextBox txt_preco;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- QueFome/QueFome/Apresentação/FormProdutos.cs
+++ QueFome/QueFome/Apresentação/FormProdutos.cs
@@ -15,6 +15,166 @@ namespace QueFome
         public FormProdutos()
         {
             InitializeComponent();
+            PopularGrade();
+        }
+        private void PopularGrade()
+        {
+            string[] item = new string[4];
+            var listaProdutos = Produtos.GetListaProdutos();
+
+            foreach (Produtos produto in listaProdutos)
+            {
+                item[0] = produto.Id.ToString();
+                item[1] = produto.Grupo;
+                item[2] = produto.Nome;
+                item[3] = produto.Preco.ToString("N2");
+
+                lv_produto.Items.Add(new ListViewItem(item));
+            }
+            ListViewSort(lv_produto, 2, true);
+        }
+        private void Limpar()
+        {
+            txt_id.Clear();
+            txt_grupo.Clear();
+            txt_nome.Clear();
+            txt_preco.Clear();
+            txt_id.Focus();
+        }
+        private void btn_adicionar_Click(object sender, EventArgs e)
+        {
+            if (txt_id.Text == "")
+            {
+                MessageBox.Show("O campo ID não pode ser vazio");
+                txt_id.Focus();
+                return;
+            }
+            if (txt_grupo.Text == "")
+            {
+                MessageBox.Show("O campo Grupo não pode ser vazio");
+                txt_grupo.Focus();
+                return;
+            }
+            if (txt_nome.Text == "")
+            {
+                MessageBox.Show("O campo Nome não pode ser vazio");
+                txt_nome.Focus();
+                return;
+            }
+            if (txt_preco.Text == "")
+            {
+                MessageBox.Show("O campo Preço não pode ser vazio");
+                txt_preco.Focus();
+                return;
+            }
+
+            string[] pr = new string[4];
+            pr[0] = txt_id.Text;
+            pr[1] = txt_grupo.Text;
+            pr[2] = txt_nome.Text;
+            pr[3] = txt_preco.Text;
+
+            ListViewItem l = new ListViewItem(pr);
+            lv_produto.Items.Add(l);
+            Limpar();
+        }
+
+        private void bt_excluir_Click(object sender, EventArgs e)
+        {
+            lv_produto.Items.RemoveAt(lv_produto.SelectedIndices[0]);
+        }
+        private void obter()
+        {
+            txt_id.Text = lv_produto.SelectedItems[0].SubItems[0].Text;
+            txt_grupo.Text = lv_produto.SelectedItems[0].SubItems[1].Text;
+            txt_nome.Text = lv_produto.SelectedItems[0].SubItems[2].Text;
+            txt_preco.Text = lv_produto.SelectedItems[0].SubItems[3].Text;
+        }
+
+        private void lv_produto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(lv_produto.SelectedItems.Count > 0)
+            {
+                obter();
+            }
+        }
+        public class Produtos
+        {
+            public int Id { get; set; }
+            public string Grupo { get; set; }
+            public string Nome { get; set; }
+            public decimal Preco { get; set; }
+
+            public Produtos(int id, string grupo, string nome, decimal preco)
+            {
+                this.Id = id;
+                this.Grupo = grupo;
+                this.Nome = nome;
+                this.Preco = preco;
+            }
+            public static List<Produtos> GetListaProdutos()
+            {
+                //Instanciar um objeto Lista de produto
+
+                var listaProdutos = new List<Produtos>();
+
+                listaProdutos.Add(new Produtos(1, "Combo", "3 SANDUÍCHES + 3 MCFRITAS MÉDIAS", 66.00M));
+                listaProdutos.Add(new Produtos(2, "Combo", "MCOFERTA MÉDIA CLÁSSICA + NUGGETS 4 OU CHEESEBURGUER OU MCFLURRY", 34.90M));
+                listaProdutos.Add(new Produtos(3, "Combo", "2 MCOFERTAS MÉDIAS", 52.90M));
+                listaProdutos.Add(new Produtos(4, "Combo", "SANDUÍCHE + BEBIDA", 19.90M));
+                listaProdutos.Add(new Produtos(5, "SANDUÍCHE", "BRABO BACON SALAD", 33.90M));
+                listaProdutos.Add(new Produtos(6, "SANDUÍCHE", "BIG TASTY", 34.90M));
+                listaProdutos.Add(new Produtos(7, "SANDUÍCHE", "DUPLO QUARTERÃO", 33.90M));
+                listaProdutos.Add(new Produtos(8, "SANDUÍCHE", "DUPLO CHEDDAR MCMELT", 33.90M));
+                listaProdutos.Add(new Produtos(9, "SANDUÍCHE", "Mac Checken", 23.90M));
+                listaProdutos.Add(new Produtos(10, "SANDUÍCHE", "DUPLO BURGER BACON", 24.90M));
+                listaProdutos.Add(new Produtos(12, "REFRIGERANTE", "Coca-Cola 350ml", 5.00M));
+                listaProdutos.Add(new Produtos(13, "REFRIGERANTE", "Coca-Cola 0% 350ml", 5.00M));
+                listaProdutos.Add(new Produtos(14, "REFRIGERANTE", "Coca-Cola 600ml", 7.00M));
+                listaProdutos.Add(new Produtos(15, "REFRIGERANTE", "Guaraná 350ml", 5.00M));
+                listaProdutos.Add(new Produtos(16, "REFRIGERANTE", "Guaraná 0% 350ml", 5.00M));
+                listaProdutos.Add(new Produtos(17, "REFRIGERANTE", "Guaraná 600ml", 7.00M));
+                listaProdutos.Add(new Produtos(18, "REFRIGERANTE", "Suco 350ml", 6.00M));
+                listaProdutos.Add(new Produtos(19, "REFRIGERANTE", "Suco 500ml", 8.50M));
+
+                return listaProdutos;
+            }
+        }
+        private void ListViewSort(ListView listView, int coluna, bool ascendente)
+        {
+            if(listView == null) return;
+
+            ListView lstView = listView as ListView;
+            int col = coluna;
+
+            bool asc = ascendente;
+            var items = lstView.Items.Cast<ListViewItem>().ToList();
+
+            var numerico = true;
+            foreach (ListViewItem item in lv_produto.Items)
+            {
+                if (!IsNumeric(item.SubItems[col].Text))
+                {
+                    numerico = false;
+                    break;
+                }
+            }
+            var sorted = new List<ListViewItem>();
+            if (numerico)
+                sorted = asc ? items.OrderBy(x => Convert.ToDecimal(x.SubItems[col].Text)).ToList() :
+                  items.OrderByDescending(x => Convert.ToDecimal(x.SubItems[col].Text)).ToList();
+            else
+                //classificar como string
+                sorted = asc ? items.OrderBy(x => x.SubItems[col].Text).ToList() :
+                items.OrderByDescending(x => x.SubItems[col].Text).ToList();
+
+            lstView.Items.Clear();
+            lstView.Items.AddRange(sorted.ToArray());
+        }
+        public static bool IsNumeric(string Text)
+        {
+            decimal result;
+            return (decimal.TryParse(Text, out result));
         }
     }
 }
